cb475ea81bb3933746a64661edaef726c5716552
Dmambo <mambotrader0@gmail.com>
2023-03-09 22:44:27 +0530

    Fixed the list

--- my_list.rb
+++ my_list.rb
@@ -3,11 +3,19 @@ require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
 
-  def initialize(list)
-    @list = list
+  def initialize(*args)
+    @list = []
+    args.each { |arg| @list << arg }
   end
 
-  def each(&block)
-    @list.each(&block)
+  def each
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    @list
   end
 end
